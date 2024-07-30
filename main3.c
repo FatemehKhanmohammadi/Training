83e8c0943d2484673d2561e0c206b3afab622b5b
FatemehKhanmohammadi <khanmohammadi.fa1378@gmail.com>
2024-07-30 17:59:31 +0330

    Add files via upload

---
+++ main3.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main(){
+    int x, y, sum = 0, i = 0, a = 0, b = 0, j = 0 , t = 0, q = 0; 
+    printf("Enter the first number:");
+    scanf("%d", &x);
+    while(x != 0){
+        t = x % 10;
+        if(t == 0){
+            t = 0;
+        }
+        else if(t == 1){
+            t = 9;
+        }
+        else if(t > 1){
+            t = t - 1;
+        } 
+        a = a + (t * 10^(i));
+        x = x/10;
+        i = i+1;
+    }
+    printf("Enter the second number:");
+    scanf("%d", &y); 
+    while(y != 0){
+        q = y % 10;
+        if(q == 0){
+            q = 0;
+        }
+        else if(q == 1){
+            q = 9;
+        }
+        else if(q > 1){
+            q = q - 1;
+        }
+        b = b + q * 10^j;
+        y = y/10;
+        j = j+1;
+    } 
+    sum = a + b;
+    printf("The sum equals to = %d, %d, %d ", a, b, sum);
+
+    return 0;
+
+}
