b5d9bef0dfd9e40c6b87deb8a9d88de0c20d15bf
FatemehKhanmohammadi <khanmohammadi.fa1378@gmail.com>
2024-07-30 12:20:08 +0330

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+// Sini chay
+#include<stdio.h>
+int main(){
+    int n, m, t;
+    printf("The number of the guests:");
+    scanf("%d", &n);
+    printf("The number of the guests drinking 2 cups of te:");
+    scanf("%d", &m); 
+    t = (n+m)/9 + 1;
+    printf("Number of Mohsen's sweep =%d", t);
+    return 0; 
+}
