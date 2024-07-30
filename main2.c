851c66a31af1005eb41731e021b19ec1026fe648
FatemehKhanmohammadi <khanmohammadi.fa1378@gmail.com>
2024-07-30 15:18:36 +0330

    Add files via upload

---
+++ main2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n, i, t;
+    int m = 0;
+    printf("Enter the number of trips: ");
+    scanf("%d", &n);
+    for(i=0; i<n; i++){
+        printf("Enter the number of passengers: "); 
+        scanf("%d", &t);
+        if(t == 11){
+             m = m +1;
+        }  
+    
+    }
+    printf("The good trips number = %d", m);
+    return 0;
+
+}
